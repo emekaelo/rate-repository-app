9b6f3438e0c1b183fa1a6e7ebda2ffa2cdd6b0ab
emekaelo <devemekaelo@gmail.com>
2024-04-24 16:56:39 +0100

    Exercise 10.12: environment variables completed

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,9 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import Constants from "expo-constants";
 
 
 const createApolloClient = () => {
     return new ApolloClient({
-        uri: 'http://192.168.1.100:4000/graphql',
+        uri: Constants.expoConfig.extra.apolloUri,
         cache: new InMemoryCache(),
     });
 };
